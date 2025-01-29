f064db0eb1bcab9d8b3ed04c4c05977f9156f5ed
eriksgda <erik.ksgda@gmail.com>
2025-01-29 20:31:21 -0300

    feat: add entity Order file

---
+++ src/main/java/com/example/Restaurant/models/Order.java
@@ -0,0 +1,54 @@
+package com.example.Restaurant.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "orders")
+@Table(name = "orders")
+public class Order {
+    @Id
+    private UUID id;
+
+    private String description;
+
+    private String status;
+
+    private LocalDateTime creationDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+}
