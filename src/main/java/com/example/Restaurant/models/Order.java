bc2641648a351133f59c8be2b45e10d4d45a288f
eriksgda <erik.ksgda@gmail.com>
2025-01-29 20:37:23 -0300

    fix: remove blank lines

--- src/main/java/com/example/Restaurant/models/Order.java
+++ src/main/java/com/example/Restaurant/models/Order.java
@@ -3,7 +3,6 @@ package com.example.Restaurant.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
